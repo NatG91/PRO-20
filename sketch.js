a35cd3113c50381d5da6ddfa733c4611d4cb5379
NatG91 <68413475+NatG91@users.noreply.github.com>
2020-07-31 11:46:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,8 @@ Weight=random(400,1500);
 
   Car=createSprite(50,200,50,50)
   Wall=createSprite(1500,200,60,height/2);
-Car.veloctiyX=Speed;
+Car.velocityX=Speed;
+Car.shapeColor=("white");
 Wall.shapeColor=(80,80,80);
 }
 
@@ -21,22 +22,22 @@ function draw() {
   background(255,255,255);  
   if (Wall.x-Car.x<(Car.width+Wall.width)/2)
   {
-    Car.veloctiyX=0;
+    Car.velocityX=0;
     var Deformation=0.5 * Weight * Speed * Speed/22509;
     if (Deformation>180)
     {
       Car.shapeColor=color(255,0,0);
     }
 
-  if (Deformation<180 && Deformation>180)
+  if (Deformation<180 && Deformation>100)
   {
     Car.shapeColor=color(230,230,0);
   }
 
-  if (Deformation,180)
+  if (Deformation<100)
   {
     Car.shapeColor=color(0,255,0);
   }
+  }
   drawSprites();
-}
 }
